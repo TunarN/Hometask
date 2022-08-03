32e1b709103f9e8ff66d63a89abe79060b12e0c9
TunarN <tunarzn@code.edu.az>
2022-08-03 16:07:22 +0400

    HomeTask

---
+++ Homework/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = { 1, 2, 3, 4, 5, 6 };
+            int min = 5;
+                for (int i = 0; i < num.Length; i++)
+            {
+                if (num[i]<min)
+                {
+                    min = num[i];
+                }
+            }
+            Console.WriteLine(min);
+        }
+    }
+}
